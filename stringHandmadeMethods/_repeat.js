01cc5aea56a4a9a5d5bf91fb06c824829405f233
Igor Oliinyk <mrfinansist1@gmail.com>
2019-06-03 21:17:15 +0300

    new format addition

--- stringHandmadeMethods/_repeat.js
+++
@@ -1,9 +0,0 @@
-export default function _repeat(str, count) {
-    let newStr = '';
-    if (count) {
-        for(let i = 0; i < count; i++) {
-        newStr += str;
-        }
-    }
-    return newStr;
-}
