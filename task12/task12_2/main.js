fd2598bbca46564e891992dc305a7b7d7bebbd7f
Igor Oliinyk <mrfinansist1@gmail.com>
2019-06-07 13:12:58 +0300

    task12: add some tasks

--- task12/task12_2/main.js
+++ task12/task12_2/main.js
@@ -1,46 +1,62 @@
-/* @~@@*@@ */
+
 function convertExpToNum(expression) {
     return expression
+        .replace(/@@@@@@@@@/g, 9)
+        .replace(/@@@@@@@@/g, 8)
+        .replace(/@@@@@@@/g, 7)
+        .replace(/@@@@@@/g, 6)
+        .replace(/@@@@@/g, 5)
+        .replace(/@@@@/g, 4)
+        .replace(/@@@/g, 3)
+        .replace(/@@/g, 2)
         .replace(/@/g, 1)
         .replace(/~~/g, 0)
-        .replace(/0(~)/g, '00');
+        .replace(/0(~)/g, '00')
+        .replace(/~/g, '');
 }
 
-function sumMembers(el) {
-    let acc = 0,
-        modEl = el,
-        resSum = '';
-        
-    if (modEl != 0 ) {
-        while (modEl > 0) {
-            let curr = modEl % 10;
-
-            if (curr !=0) {
-                acc += curr;
-            } else {
-                resSum = '0' + acc +  resSum;
-                acc = 0;
-            }
-            modEl = Math.floor(modEl / 10)
-        }
-        resSum = acc + resSum;
-    } else {
-        resSum = el;
-    } 
-    return resSum;
+function convertNumtoExp(expression) {
+    return expression
+        .replace(/@@@@@@@@@/g, 9)
+        .replace(/@@@@@@@@/g, 8)
+        .replace(/@@@@@@@/g, 7)
+        .replace(/@@@@@@/g, 6)
+        .replace(/@@@@@/g, 5)
+        .replace(/@@@@/g, 4)
+        .replace(/@@@/g, 3)
+        .replace(/@@/g, 2)
+        .replace(/@/g, 1)
+        .replace(/~~/g, 0)
+        .replace(/0(~)/g, '00')
+        .replace(/~/g, '');
 }
 
 function calcLikeIncas(expression) {
     let expToNum,
         arrNum;
+        members,
+        sign
+        sum
+        NumToExp;
 
-    expToNum = convertExpToNum(expression);
-    arrNum = expToNum.split('~');
-    let newarrNum = arrNum.map((el) => {
-        return sumMembers(el);
-    })
+  expToNum = convertExpToNum(expression);
+   
+        members = expToNum.match(/\d+~?\d+/g),
+        sign = expToNum.match(/[\+\-\*\/]/g)
     
-    console.log(newarrNum.join(''))
+if (sign == '+') {
+    sum = 
+}
+if (sign == '-') {
+    sum = 
+}
+if (sign == '/') {
+    sum = 
+}
+if (sign == '*') {
+sum = 
 }
 
+return NumToExp = convertNumtoExp(sum);
+}
 calcLikeIncas('@@@~~@~@@@~~@+@@@~~@');
